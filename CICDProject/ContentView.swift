ee3fb43d37276fbb628d3eea35ab6874b42d7c99
jaydip-7span <jaydip@7span.com>
2025-03-14 12:26:19 +0530

    - Fontweight added

--- CICDProject/ContentView.swift
+++ CICDProject/ContentView.swift
@@ -13,6 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
+                .fontWeight(.medium)
             Text("Hello, world!")
         }
         .padding()

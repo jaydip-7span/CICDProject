b40245c9f8b5130e46e37aea74ed89cd413a0bc5
jaydip-7span <jaydip@7span.com>
2025-03-14 12:22:13 +0530

    -text alignment left side set

--- CICDProject/ContentView.swift
+++ CICDProject/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            
+                .multilineTextAlignment(.leading)
             Text("Hello, world!")
         }
         .padding()

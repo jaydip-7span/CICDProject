e580befed7bdb6244fc922a3e0f65650d3bb0403
mukund <mukund@7span.com>
2025-03-14 09:00:39 +0530

    Initial Commit

---
+++ CICDProject/CICDProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  CICDProjectApp.swift
+//  CICDProject
+//
+//  Created by Jaydip Jadav on 14/03/25.
+//
+
+import SwiftUI
+
+@main
+struct CICDProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CICDProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CICDProject
+//
+//  Created by Jaydip Jadav on 14/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}

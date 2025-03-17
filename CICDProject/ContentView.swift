e1a4df0d746b47a7c9af53bc4228a3b4b13e20c5
jaydip-7span <jaydip@7span.com>
2025-03-17 16:03:14 +0530

    Remove text

--- CICDProject/ContentView.swift
+++ CICDProject/ContentView.swift
@@ -14,7 +14,6 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
                 .fontWeight(.medium)
-            Text("Hello, world!123hjhjhjkhkj")
             Text("Hello, world!")
         }
         .padding()
